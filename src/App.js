2773dd973492446bae804f2aadc10fa012f5963c
Doan Ngoc Thach <thachdn@ftech.ai>
2021-04-27 16:22:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from 'ckeditor5-classic-with-mathtype';
+// import ClassicEditor from 'ckeditor5-classic-with-mathtype';
 // import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
 // import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-// const str = `Nguyên tử vàng có 79 electron ở vỏ nguyên tử. Điện tích hạt nhân của nguyên tử vàng là;A. +79\nB. -79\nC.  <math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>-</mo><mn>1</mn><mo>,</mo><mn>266</mn><mo>.</mo><msup><mn>10</mn><mrow><mo>-</mo><mn>17</mn></mrow></msup><mo> </mo><mi>C</mi></math>\nD.  <math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>+</mo><mn>1</mn><mo>,</mo><mn>266</mn><mo>.</mo><msup><mn>10</mn><mrow><mo>-</mo><mn>17</mn></mrow></msup><mo> </mo><mi>C</mi></math>;D.  <math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>+</mo><mn>1</mn><mo>,</mo><mn>266</mn><mo>.</mo><msup><mn>10</mn><mrow><mo>-</mo><mn>17</mn></mrow></msup><mo> </mo><mi>C</mi></math>;Giải Thích\nĐáp án D\nĐiện tích hạt nhân là  <math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mn>79</mn><mo>.</mo><mo> </mo><mn>1</mn><mo>,</mo><mn>602</mn><mo>.</mo><msup><mn>10</mn><mrow><mo>-</mo><mn>19</mn></mrow></msup><mo> </mo><mo>=</mo><mo>+</mo><mn>1</mn><mo>,</mo><mn>266</mn><mo>.</mo><msup><mn>10</mn><mrow><mo>-</mo><mn>17</mn></mrow></msup><mo> </mo><mi>C</mi></math> , hoặc kí hiệu là 79+.`
-
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+const str = `+ Mệnh đề” Nếu  !!![equation](<math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mo>-</mo><mfrac><mrow><mn>1</mn></mrow><mrow><mn>2</mn></mrow></mfrac><msup><mrow><mi>B</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup></math>)!!!  thì  !!![equation](<math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><mi>B</mi></math>)!!!  là trung điểm của đoạn AC sai vì  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mo>-</mo><mfrac><mrow><mn>1</mn></mrow><mrow><mn>2</mn></mrow></mfrac><msup><mrow><mi>B</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>⇒</mo></math>   <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><mi>A</mi></math>  là trung điểm BC.↵ ta suy ra  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>C</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>.</mo></math>   sai vì  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>-</mo><mn>3</mn><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>⇒</mo></math>   <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>C</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mo>-</mo><mn>4</mn><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup></math> .↵+ Mệnh đề "Vì  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mo>-</mo><mn>2</mn><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>+</mo><mn>5</mn><msup><mrow><mi>A</mi><mi>D</mi></mrow><mrow><mo>→</mo></mrow></msup></math>  nên bốn điểm  cùng thuộc một mặt phẳng" là đúng vì theo định lý về sự đồng phẳng của 3 véctơ.↵+ Mệnh đề "từ  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mn>3</mn><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup></math>  ta suy ra  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>B</mi><mi>A</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mo>-</mo><mn>3</mn><msup><mrow><mi>C</mi><mi>A</mi></mrow><mrow><mo>→</mo></mrow></msup></math> " là sai vì  <math xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mml="http://www.w3.org/1998/Math/MathML"><msup><mrow><mi>A</mi><mi>B</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mn>3</mn><msup><mrow><mi>A</mi><mi>C</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>⇒</mo><msup><mrow><mi>B</mi><mi>A</mi></mrow><mrow><mo>→</mo></mrow></msup><mo>=</mo><mn>3</mn><msup><mrow><mi>C</mi><mi>A</mi></mrow><mrow><mo>→</mo></mrow></msup></math>`
 const convertToText = str => {
   const splitStr = str?.split(/<p>(.*?)<\/p>/g);
   return splitStr?.filter(e => e != "").join(`\n`) || ''
@@ -12,15 +12,17 @@ const convertToText = str => {
 const converToXml = xml => {
   return xml?.split('\n').map(e => `<p>${e}</p>`).join("") || ''
 }
-
 const App = () => {
   const [data, setdata] = useState('')
-  console.log(data)
+  // console.log(data)
 
   return (
     <CKEditor
-      editor={ClassicEditor}
+      editor={Editor}
       config={{
+        ckfinder: {
+          uploadUrl: 'http://localhost:8000/upload',
+        },
         toolbar: {
           items: [
             'heading',
@@ -38,12 +40,17 @@ const App = () => {
             'blockQuote',
             'undo',
             'redo',
+            "Image",
+            "ImageCaption",
+            "ImageStyle",
+            "ImageToolbar",
           ],
         },
       }}
       onChange={(event, editor) => {
         const data = editor.getData();
-        setdata(convertToText(data))
+        // console.log(converToXml(data))
+        setdata(data);
       }}
       data={converToXml(data)}
       onReady={(editor) => {
